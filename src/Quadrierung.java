1341127c2553083cbd0bb97712eb020253d7e3e2
Sara Katharina Horvat <01310623@edu.campus02.at>
2022-12-11 15:17:57 +0100

    Class renamed.

--- src/Quadrierung.java
+++ src/Quadrierung.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FirstLoveContinued {
+public class Quadrierung {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Rehearsal of Class 'First Love'");
